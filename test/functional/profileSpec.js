69ec9afd982797d2eb2f49190197714c5e2a0cb9
guifouche <fouchegui@gmail.com>
2018-01-19 11:12:10 +0100

    Nightwatch

---
+++ test/functional/profileSpec.js
@@ -0,0 +1,16 @@
+var config = require("../../nightwatch.conf");
+
+module.exports = {
+  'Profile page show displays user informations ': function (client) {
+    client
+      .url('http://localhost:3000/profile')
+      .assert('input[name="lastname"]', 'RAMBO')
+      .assert('input[name="firstname"]', 'John')
+      .assert('input[name="nickname"]', 'jrambo')
+      .assert('input[name="email"]', 'john.rambo@rocky.tld')
+      .assert('input[name="password"]', 'p@ssword')
+      .assert('input[name="password_confirm"]', 'p@ssword')
+      .assert('input[name="birthdate"]', '06/07/1946')
+      .end();
+  },
+};
