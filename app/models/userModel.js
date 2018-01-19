deb4a0dd20aba287615e971439dc56544f136aef
Doshibu <guillaume.bonhommeau@viacesi.fr>
2018-01-19 14:55:57 +0100

    email format validation

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,46 +1,60 @@
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 
-var userSchema = mongoose.Schema({
-
-    local            : {
-        email       : { type : String, required : true },
-        password    : { type : String, required : true },
-        lastname    : String,
-        firstname   : String,
-        nickname    : String,
-        birthdate   : Date,
-        gender      : String,
-        presentation: String,
-        picture     : String
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
-
-});
-
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
 };
 
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
+const userSchema = mongoose.Schema({
+  local: {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: 'Email address is required',
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: { type: String, required: true },
+    lastname: String,
+    firstname: String,
+    nickname: String,
+    birthdate: Date,
+    gender: String,
+    presentation: String,
+    picture: String
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  },
+  twitter: {
+    id: String,
+    token: String,
+    displayName: String,
+    username: String
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  }
 
-module.exports = mongoose.model('User', userSchema);
+})
+
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
+
+module.exports.schema = userSchema
+
+module.exports.model = mongoose.model('User', userSchema)
